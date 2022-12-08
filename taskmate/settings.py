1a6c8a1da50a2f653b24c2986f5b89f56eb36ef3
Shubham Sarda <shubhamsarda99@yahoo.com>
2022-12-08 22:50:48 +0530

    Domain Updates

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://taskmate.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ['https://taskmate.up.railway.app']
 
 # Application definition
 
